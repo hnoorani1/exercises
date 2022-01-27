dea1545e46fc82a13f49afaedaf0d22874244c31
Hassan <hnoorani9@gmail.com>
2022-01-27 16:26:40 -0600

    Final
    Final exercise

--- exercise3.py
+++ exercise3.py
@@ -9,7 +9,9 @@ and the time of non-200 response in human-readable format in PST timezone
 Please use Jmeter_log1.jtl, Jmeter_log2.jtl as input files for testing out your script
 (the files have .jtl extension but the format is  CSV)."""
 
-jmeter_records_df = pd.read_csv('Jmeter_logs/Jmeter_log1.jtl')
+jmeter_log_files = ('Jmeter_logs/Jmeter_log1.jtl')
+
+jmeter_records_df = pd.read_csv(jmeter_log_files)
 not_two_hundred_codes_df = jmeter_records_df[jmeter_records_df['responseCode']!=200]
-log_specific_headers = not_two_hundred_codes_df['label']['responseCode']['responseMessage']['failureMessage']['timeStamp']
+log_specific_headers = not_two_hundred_codes_df[['label', 'responseCode', 'responseMessage', 'failureMessage', 'timeStamp']]
 print (log_specific_headers)
